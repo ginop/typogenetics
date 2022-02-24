73d71a8aac5f8d412d34df0ed6f42d6e3a0bfbf1
Gino Perrotta <ginommperrotta@gmail.com>
2022-02-24 08:24:13 -0500

    Log random population count to file

--- scripts/population_simulation.py
+++ scripts/population_simulation.py
@@ -2,14 +2,15 @@ import collections
 import itertools
 import random
 
+from tqdm import tqdm
+
 from typogenetics.strand import Strand, BASES
 from typogenetics.enzyme import Enzyme
 
 
 class Population(collections.Counter):
     def sample(self):
-        assert self.total > 0
-        random_index = random.randrange(self.total)
+        random_index = random.randrange(self.total())
         for species, count in self.items():
             random_index -= count
             if random_index <= 0:
@@ -21,24 +22,25 @@ class Population(collections.Counter):
         else:
             raise AssertionError
 
-    @property
-    def total(self):
-        return sum(self.values())
 
+def population_simulation(log_file=None):
+    strands = Population()
+    enzymes = Population()
 
-def population_simulation():
-    original_strand = "".join(random.choices(tuple(BASES), k=random.randint(50, 101)))
-    strands = Population([original_strand])
-    enzymes = Population(str(enzyme) for enzyme in Enzyme.make_from_genes(original_strand))
+    for _ in range(1):
+        original_strand = "".join(random.choices(tuple(BASES), k=random.randint(50, 101)))
+        strands[original_strand] += 1
+        for enzyme in Enzyme.make_from_genes(original_strand):
+            enzymes[str(enzyme)] += 1
 
-    for n_gen in itertools.count():
-        print(f"Gen {n_gen}: \n"
-              f"{strands.total} strands in {len(strands)} species and \n"
-              f"{enzymes.total} enzymes in {len(enzymes)} species")
+    for n_gen in tqdm(itertools.count()):
+        # print(f"Gen {n_gen}: \n"
+        #       f"{strands.total} strands in {len(strands)} species and \n"
+        #       f"{enzymes.total} enzymes in {len(enzymes)} species")
 
         # If no enzymes, create more
-        if enzymes.total == 0:
-            print("No enzymes available")
+        if enzymes.total() == 0:
+            # print("No enzymes available")
             strand = strands.sample()
             for enzyme in Enzyme.make_from_genes(strand):
                 enzymes[str(enzyme)] += 1
@@ -48,12 +50,12 @@ def population_simulation():
         # Get one random enzyme and one strand
         enzyme = Enzyme(enzymes.sample().split(" - "))
         strand = Strand.make_from_code(strands.sample())
-        print(strand, enzyme, sep="\n")
+        # print(strand, enzyme, sep="\n")
 
         # Find preferred binding site
         binding_options = [n for n, unit in enumerate(strand) if unit.facing == enzyme.binding_preference]
         if not binding_options:
-            print("No binding site found")
+            # print("No binding site found")
             strands[str(strand)] += 1
             enzymes[str(enzyme)] += 1
             continue
@@ -67,8 +69,16 @@ def population_simulation():
             for enzyme in Enzyme.make_from_genes(str(strand)):
                 enzymes[str(enzyme)] += 1
 
-        print(strands.most_common(5))
+        # print(strands.most_common(5))
+
+        if log_file is not None:
+            log_file.write(
+                f"(total: {strands.total()}), " +
+                ", ".join(f"({species}: {count})" for species, count in strands.most_common(10)) +
+                "\n"
+            )
 
 
 if __name__ == '__main__':
-    population_simulation()
+    with open("population_log.csv", "w") as file:
+        population_simulation(file)
